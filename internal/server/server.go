2ec1912c33964739897345285770fec9b4d122ee
Matyjash <lengom4w@gmail.com>
2025-09-16 23:26:50 +0200

    feat: add GetCpuInfo request handler

--- internal/server/server.go
+++ internal/server/server.go
@@ -28,3 +28,22 @@ func (s *Server) GetMemoryUsage(ctx context.Context, req *pb.MemoryUsageReq) (*p
 		UsedB:  memoryUsage.UsedB,
 	}, nil
 }
+
+func (s *Server) GetCpuInfo(ctx context.Context, req *pb.CpuInfoReq) (*pb.CpuInfoRes, error) {
+	cpuInfo, err := s.metrigo.GetCpuInfo()
+	if err != nil {
+		return nil, err
+	}
+
+	var cpuInfosRes []*pb.CpuInfo
+	for _, info := range cpuInfo {
+		cpuInfoPb := &pb.CpuInfo{
+			Id:           info.ID,
+			UsagePercent: float32(info.UsagePercent),
+			Frequency:    float32(info.FrequencyMhz),
+		}
+		cpuInfosRes = append(cpuInfosRes, cpuInfoPb)
+	}
+
+	return &pb.CpuInfoRes{CpuInfo: cpuInfosRes}, nil
+}
